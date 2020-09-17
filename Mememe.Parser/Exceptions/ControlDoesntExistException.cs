3ea5a844b5fb4bb2a757121b5a6b7a6e5bfca0d3
NiJeTi <nijeti.inc@gmail.com>
2020-09-17 13:18:55 +0300

    Additional exceptions

---
+++ Mememe.Parser/Exceptions/ControlDoesntExistException.cs
@@ -0,0 +1,9 @@
+﻿using OpenQA.Selenium;
+
+namespace Mememe.Parser.Exceptions
+{
+    public class ControlDoesntExistException : NoSuchElementException
+    {
+        public ControlDoesntExistException(Control control) : base($"{control} doesn't exist") { }
+    }
+}
---
+++ Mememe.Parser/Exceptions/WebDriverUninitializedException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Mememe.Parser.Exceptions
+{
+    public class WebDriverUninitializedException : NullReferenceException
+    {
+        public WebDriverUninitializedException() : base("Web-driver hasn't been initialized") { }
+    }
+}
