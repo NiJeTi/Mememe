d634dced83dca5889f2c335a4a59e45767ddefc2
NiJeTi <nijeti.inc@gmail.com>
2020-09-17 13:20:00 +0300

    NineGag entity model

---
+++ Mememe.NineGag/Models/Article.cs
@@ -0,0 +1,14 @@
+﻿namespace Mememe.NineGag.Models
+{
+    public class Article
+    {
+        public Article(string title)
+        {
+            Title = title;
+        }
+
+        public string Title { get; }
+        public string? Image { get; set; }
+        public string? Video { get; set; }
+    }
+}
